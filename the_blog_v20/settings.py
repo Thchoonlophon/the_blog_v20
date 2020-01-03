c0165c8eafdcfdfc9c226228878bb724d9c13cef
Thchoonlophon <qianchenjin192518@gmail.com>
2020-01-03 16:30:57 +0800

    change the way to connect mysql

--- the_blog_v20/settings.py
+++ the_blog_v20/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import sys
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 # 导入网站个人信息，非通用信息
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
