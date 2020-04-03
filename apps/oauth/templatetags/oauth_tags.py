bc035cae75849d17f8f6bab05ae8084a41a4fbeb
thchoonlophon <qianchenjin192518@gmail.com>
2020-04-03 13:56:35 +0800

    Update oauth_tags.py

--- apps/oauth/templatetags/oauth_tags.py
+++ apps/oauth/templatetags/oauth_tags.py
@@ -7,25 +7,25 @@ register = template.Library()
 
 @register.inclusion_tag('oauth/tags/user_avatar.html')
 def get_user_avatar_tag(user):
-    '''返回用户的头像，是一个img标签'''
+    """返回用户的头像，是一个img标签"""
     return {'user': user}
 
 
 @register.simple_tag
 def http_to_https(link):
-    '''将http链接替换成https，目的是将微博的头像图床地址换成HTTPS'''
+    """将http链接替换成https，目的是将微博的头像图床地址换成HTTPS"""
     return link.replace('http://', 'https://')
 
 
 @register.simple_tag
 def get_user_link(user):
-    '''
+    """
     获取认证用户的link，并判断用户是哪种认证方式（Github，Weibo，邮箱）
     参考 get_social_accounts(user) 的用法
     :param user: 一个USER对象
     :return: 返回用户的link和注册方式以及是否验证过邮箱地址,link的优先顺序是user.link，其次是github主页，
             考虑到很多人不愿意展示微博主页，所以不展示weibo主页
-    '''
+    """
     info = {
         'link': None,
         'provider': None,
